e572787fc458cb26659ec1b35ce0c05c7880624f
jamaneffa <jamaneffa@yahoo.com>
2024-02-20 13:25:08 -0300

    clase17

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+const {resolve} = require('path')
+
+const port = 2020	
+
+app.listen(port, () => console.log (`Starting Server in http://localhost:${port}`))
+
+app.use(express.static(resolve('./public')))
+
+app.get('/', (req, res) => res.sendFile(resolve('./views/home.html')))
